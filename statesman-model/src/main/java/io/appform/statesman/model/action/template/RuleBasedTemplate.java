0cf5e46663870052fdde4b8260d3b69ec9dc5f81
Priya Dharshini <priya.p.dharshini@gmail.com>
2021-05-30 23:39:14 +0530

    Adds test

--- statesman-model/src/main/java/io/appform/statesman/model/action/template/RuleBasedTemplate.java
+++ statesman-model/src/main/java/io/appform/statesman/model/action/template/RuleBasedTemplate.java
@@ -1,10 +1,12 @@
 package io.appform.statesman.model.action.template;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class RuleBasedTemplate {
     public static enum Type {
         EVALUATED,
